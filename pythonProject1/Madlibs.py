aa72c96db105c7e87d578a4e094415537473b5e5
Lizziem <77862680+lizzie-m@users.noreply.github.com>
2021-04-30 01:12:34 +0100

    Add files via upload

---
+++ pythonProject1/Madlibs.py
@@ -0,0 +1,9 @@
+# 2021 Python project 1
+# A cake baking Madlib project
+
+adj = input("Dessert: ")
+ingredient1 = input("ingredient 1: ")
+ingredient2= input("ingredient 2:")
+ingredient3 = input("ingredient 3:")
+madlib = f" To bake a {adj} you must add {ingredient1} \n, {ingredient2} and don't forget the {ingredient3}!"
+print(madlib)
